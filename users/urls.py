df70cf03014339272bfff170f32abd4536e5df5b
Arslan <91959072+rasulow@users.noreply.github.com>
2025-01-02 11:20:32 +0500

    user service swagger title

--- users/urls.py
+++ users/urls.py
@@ -6,9 +6,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="User Service",
       default_version='v1',
-      description="Test description",
+      description="Emugallym User Service",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
