3014652a3dd23b1253937d8ef913a32e5ffe885d
Arslan <91959072+rasulow@users.noreply.github.com>
2025-01-09 23:40:12 +0500

    adding comma

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -23,7 +23,7 @@ class BookSerializer(serializers.ModelSerializer):
         model = models.Book
         fields = ('title', 'description', 'authors', 'genres', 'price', 
                   'discount', 'price_discount', 'file', 'cover', 'pages', 'reading_time',
-                  'published_year', 'age_restriction', 'date_of_writing', 'isbn', 'translator'
+                  'published_year', 'age_restriction', 'date_of_writing', 'isbn', 'translator',
                   'book', 'img', 'ext', 'book_size', 'order', 'slug', 'is_active')
         read_only_fields = ('slug', 'created_at', 'updated_at')
     
