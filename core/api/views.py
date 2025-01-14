10fe6316f22bc1c202fbc5b8b4128ec34ee16f77
Arslan <91959072+rasulow@users.noreply.github.com>
2025-01-14 15:41:49 +0500

    lesson chunck file

--- core/api/views.py
+++ core/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, filters
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend 
 from rest_framework.response import Response
 from drf_yasg import openapi
@@ -99,6 +100,7 @@ class TopicViewSet(viewsets.ModelViewSet):
     ordering_fields = ['order', 'created_at']
     
     
+    
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = models.Lesson.objects.all()
     serializer_class = serializers.LessonSerializer
@@ -108,6 +110,44 @@ class LessonViewSet(viewsets.ModelViewSet):
     filterset_fields = ['course__slug', 'topic__slug']
     search_fields = ['title']
     ordering_fields = ['order', 'created_at']
+
+    @action(detail=True, methods=['get'], url_path='material')
+    def retrieve_material(self, request, slug=None):
+        lesson = self.get_object()
+
+        if lesson.type == 'video' and lesson.material:
+            video_path = lesson.material.path
+
+            try:
+                file_size = os.path.getsize(video_path)
+                range_header = request.headers.get('Range', '').strip()
+
+                if not range_header:
+                    response = FileResponse(open(video_path, 'rb'), content_type='video/mp4')
+                    response['Content-Length'] = str(file_size)
+                    return response
+
+                range_match = re.match(r'bytes=(\d+)-', range_header)
+                if range_match:
+                    start_byte = int(range_match.group(1))
+                else:
+                    start_byte = 0
+
+                chunk_size = 8192
+
+                file = open(video_path, 'rb')
+                file.seek(start_byte)
+
+                response = HttpResponse(FileWrapper(file, chunk_size), status=206, content_type='video/mp4')
+                response['Content-Range'] = f'bytes {start_byte}-{file_size - 1}/{file_size}'
+                response['Content-Length'] = str(file_size - start_byte)
+
+                return response
+
+            except FileNotFoundError:
+                raise Http404("Video not found")
+
+        return Http404("Material not available or not a video")
     
     
 class LevelViewSet(viewsets.ModelViewSet):
