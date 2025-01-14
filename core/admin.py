2ce3b865d194b11dd0f0dc513170cf683bb6fb41
Arslan <91959072+rasulow@users.noreply.github.com>
2025-01-14 23:17:01 +0500

    excludes in admin side

--- core/admin.py
+++ core/admin.py
@@ -8,6 +8,7 @@ from . import models
 @admin.register(models.Category)
 class CategoryAdmin(ModelAdmin):
     list_display = ('title', 'is_active', 'created_at', 'updated_at')
+    exclude = ('created_at', 'updated_at', 'slug',)
 
 
 @admin.register(models.Level)
@@ -23,6 +24,7 @@ class LanguageAdmin(ModelAdmin):
 @admin.register(models.Course)
 class CourseAdmin(ModelAdmin):
     list_display = ('title', 'description', 'get_user', 'price', 'course_duration', 'is_active')
+    exclude = ('created_at', 'updated_at', 'slug', 'hours', 'minutes', 'seconds')
     
     def get_user(self, obj):
         try:
@@ -34,14 +36,17 @@ class CourseAdmin(ModelAdmin):
         except requests.RequestException as e:
             return str(e)
         
+        
             
 
 
 @admin.register(models.Topic)
 class TopicAdmin(ModelAdmin):
     list_display = ('title', 'course', 'order', 'topic_duration', 'is_active')
+    exclude = ('created_at', 'updated_at', 'slug', 'hours', 'minutes', 'seconds')
     
     
 @admin.register(models.Lesson)
 class LessonAdmin(ModelAdmin):
     list_display = ('title', 'topic', 'course', 'type', 'lesson_duration', 'is_active')
+    exclude = ('created_at', 'updated_at', 'slug', 'hours', 'minutes', 'seconds')
