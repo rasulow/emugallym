b9c7b1fb809fae758d81a80016607f1acbe4b106
Arslan <91959072+rasulow@users.noreply.github.com>
2025-01-16 16:05:59 +0500

    fix bugs

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -50,7 +50,7 @@ class ProfessionSerializer(serializers.ModelSerializer):
         read_only_fields = ['slug', 'created_at', 'updated_at'] 
     
 class UserGetSerializer(serializers.ModelSerializer):
-    # profession = ProfessionSerializer(read_only=True)
+    profession = ProfessionSerializer(read_only=True)
     
     class Meta:
         model = User
@@ -64,8 +64,8 @@ class UserGetSerializer(serializers.ModelSerializer):
                     'first_name', 
                     'last_name', 
                     'middle_name',
-                    'address'
-                    # 'profession', 
+                    'address',
+                    'profession', 
                     'type', 
                     'phone_number', 
                     'img', 
