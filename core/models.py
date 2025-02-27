d94a654537045fd6d22b36dc27b236bdabbe510a
Arslan <91959072+rasulow@users.noreply.github.com>
2025-02-27 11:37:56 +0500

    commit

--- core/models.py
+++ core/models.py
@@ -3,12 +3,8 @@ from django.db import models
 from django.utils.text import slugify
 import os
 from ckeditor.fields import RichTextField
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from .utils import normalize_time
-
-
-
 
 
 class Category(models.Model):
@@ -18,23 +14,22 @@ class Category(models.Model):
     order = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(str(uuid.uuid4()))
         super().save(*args, **kwargs)
-        
-        
+
     class Meta:
         db_table = 'category'
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
         ordering = ['-created_at']
-        
-        
+
+
 class Level(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
@@ -42,22 +37,22 @@ class Level(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(str(uuid.uuid4()))
         super().save(*args, **kwargs)
-    
+
     class Meta:
         db_table = 'level'
         verbose_name = 'Level'
         verbose_name_plural = 'Levels'
         ordering = ['-created_at']
-        
-        
+
+
 class Language(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
@@ -65,21 +60,21 @@ class Language(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(str(uuid.uuid4()))
         super().save(*args, **kwargs)
-    
+
     class Meta:
         db_table = 'languages'
         verbose_name = 'Language'
         verbose_name_plural = 'Languages'
         ordering = ['-created_at']
-    
+
 
 class Course(models.Model):
     title = models.CharField(max_length=100)
@@ -106,28 +101,27 @@ class Course(models.Model):
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(str(uuid.uuid4()))
         super().save(*args, **kwargs)
-        
+
     def course_duration(self):
         if self.hours == 0:
             return f'{self.minutes:02d}:{self.seconds:02d}'
         return f'{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}'
-        
-        
+
     class Meta:
         db_table = 'course'
         verbose_name = 'Course'
         verbose_name_plural = 'Courses'
         ordering = ['-created_at']
-    
-    
+
+
 class Topic(models.Model):
     title = models.CharField(max_length=255)
     course = models.ForeignKey(Course, related_name='topics', on_delete=models.CASCADE)
@@ -139,28 +133,27 @@ class Topic(models.Model):
     is_active = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(str(uuid.uuid4()))
         super().save(*args, **kwargs)
-        
+
     def topic_duration(self):
         if self.hours == 0:
             return f'{self.minutes:02d}:{self.seconds:02d}'
         return f'{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}'
-        
-    
+
     class Meta:
         db_table = 'topic'
         verbose_name = 'Topic'
         verbose_name_plural = 'Topics'
         ordering = ['-created_at']
-    
-    
+
+
 class Lesson(models.Model):
     TYPES = (
         ('video', 'Video'),
@@ -179,11 +172,10 @@ class Lesson(models.Model):
     is_active = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
-    
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(str(uuid.uuid4()))
@@ -193,13 +185,13 @@ class Lesson(models.Model):
                 video_path = self.material.path
                 clip = VideoFileClip(video_path)
                 duration_in_seconds = clip.duration
-                
+
                 hours = int(duration_in_seconds // 3600)
                 minutes = int((duration_in_seconds % 3600) // 60)
                 seconds = int(duration_in_seconds % 60)
-                
+
                 self.hours, self.minutes, self.seconds = normalize_time(hours, minutes, seconds)
-                
+
                 self.topic.hours, self.topic.minutes, self.topic.seconds = normalize_time(
                     self.topic.hours + self.hours,
                     self.topic.minutes + self.minutes,
@@ -216,29 +208,22 @@ class Lesson(models.Model):
 
             except Exception as e:
                 print(f"Error processing video: {e}")
-                
+
         super().save(*args, **kwargs)
-        
-        
+
     def delete(self, *args, **kwargs):
         if self.material:
             if os.path.isfile(self.material.path):
                 os.remove(self.material.path)
-        super().delete(*args, **kwargs) 
-        
+        super().delete(*args, **kwargs)
+
     def lesson_duration(self):
         if self.hours == 0:
             return f'{self.minutes:02d}:{self.seconds:02d}'
         return f'{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}'
-        
-        
+
     class Meta:
         db_table = 'lesson'
         verbose_name = 'Lesson'
         verbose_name_plural = 'Lessons'
         ordering = ['-created_at']
-        
-    
-    
-
-    
